d66df889216bbde35ff06672adac1053f9bfc8de
DeveloFer77 <fernando.proff@hotmail.com>
2021-01-18 17:54:54 -0300

    primera prueba

---
+++ docs/js/funciones.js
@@ -0,0 +1,10 @@
+jQuery(Document).ready(listo);
+
+
+
+function listo()
+{
+    jQuery(".hamb").click(function(e){e.preventDefault();
+    jQuery("header .container nav").toggleClass("open")
+    });
+}
